a0b6b31e36f9b4632479a056d36c32d5d2f9ac4b
Nabila Gudiño Ochoa <Nabila Gudio Ochoa>
2016-06-12 02:41:51 -0300

    Subida de Archivos

---
+++ base.js
@@ -0,0 +1,11 @@
+var x = document.createElement('script');
+x.src = './bower_components/jquery/dist/jquery.min.js';
+document.getElementsByTagName("head")[0].appendChild(x);
+
+var x = document.createElement('script');
+x.src = './bower_components/tiny.js/tiny.min.js';
+document.getElementsByTagName("head")[0].appendChild(x);
+
+var x = document.createElement('script');
+x.src = './bower_components/chico/dist/ui/chico.min.js';
+document.getElementsByTagName("head")[0].appendChild(x);
